dad346e50c8eb6a71f1f879f65b3c86cb25cb284
hkhoi1589 <hkhoi1589@gmail.com>
2023-05-15 10:56:34 +0700

    fix

--- index.js
+++ index.js
@@ -13,10 +13,8 @@ server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-/*
 const PORT = 8000;
 
 server.listen(PORT, () => {
 	console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
-*/
